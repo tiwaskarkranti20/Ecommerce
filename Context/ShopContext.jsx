26bb7846009db2379891e97cdcb786689b09d163
tiwaskarkranti20 <153894894+tiwaskarkranti20@users.noreply.github.com>
2024-07-07 16:12:15 +0530

    Add files via upload

---
+++ Context/ShopContext.jsx
@@ -0,0 +1,17 @@
+import React, { createContext } from "react";
+import all_product from '../Components/Assets/all_product'
+
+export const ShopContext = createContext(null);
+
+const ShopContextProvider = (props) => {
+
+    const contextvalue = {all_product};
+
+    return (
+        <ShopContext.Provider value={contextvalue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
